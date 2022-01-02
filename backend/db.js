f285952245d0bcb1ef897d2fdc71230e20a210bd
sagada <wlatmdqja@naver.com>
2022-01-02 21:46:10 +0900

    travis ci deploy

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql");
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "mysql",
-  user: "root",
-  password: "password",
-  database: "myapp",
-  port: 3306,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
 
 exports.pool = pool;

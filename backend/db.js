1725134838332a4513636dc5bf7a91389c4a1a2e
sagada <wlatmdqja@naver.com>
2022-01-03 23:55:29 +0900

    change compose

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,17 @@ const mysql = require("mysql");
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
+  // host: process.env.MYSQL_HOST,
+  // user: process.env.MYSQL_USER,
+  // password: process.env.MYSQL_ROOT_PASSWORD,
+  // database: process.env.MYSQL_DATABASE,
+  // port: process.env.MYSQL_PORT,
+
+  host: "mysql",
+  user: "root",
+  password: "password",
+  database: "myapp",
+  port: 3306,
 });
 
 exports.pool = pool;
